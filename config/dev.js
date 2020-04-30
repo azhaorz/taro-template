bd38a912aadf59d3ff79d8b3898a5d8b0053299d
Hanzhaorz <hzoliverdev@gmail.com>
2020-05-01 00:32:36 +0800

    feat: update

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,19 @@ module.exports = {
   env: {
     NODE_ENV: '"development"',
   },
+  plugins: [
+    [
+      '@tarojs/plugin-mock',
+      {
+        mocks: {
+          '/api/mock/1': {
+            name: 'hanzhaorz',
+            desc: 'author',
+          },
+        },
+      },
+    ],
+  ],
   defineConstants: {},
   mini: {},
   h5: {},
